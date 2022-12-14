685ad83be31bc66f7a94a342b22f6f6c5f9091d8
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-14 13:36:52 +0100

    Snack_2: validation - flag - for cycle

--- snack_2.js
+++ snack_2.js
@@ -5,12 +5,39 @@ comunicagli se può partecipare o no alla festa. (vietato usare includes() ) */
 // Element from DOM
 const nameElement = document.getElementById("name");
 const buttonElement = document.getElementById("button");
+const messageElement = document.getElementById("message");
 
-const partyList = ["Luca", "Mattia", "Maria", "Giovanna"];
+// Party List
+const partyList = ["luca", "mattia", "maria", "giovanna"];
 
 
 buttonElement.addEventListener('click', function(){
-    const userName = nameElement.value;
-    console.log(userName);
+
+    const userName = nameElement.value.toLowerCase().trim();
+
+    //Validation
+    if(!isNaN(userName)) {
+        messageElement.innerText = "non hai inserito un nome";
+        return;
+    }
+
+ 
+    // Flag - check list
+    let isAllowed = false;
+
+    for (let i=0; i < partyList.length; i++) {
+        const currentName = partyList[i];
+
+        if (currentName === userName) {
+            console.log('is allowed : vero');
+            isAllowed = true;
+        }
+    }
+
+    if (isAllowed) {
+        messageElement.innerText = "Il tuo nome è nella lista, benvenuto al party!";
+    } else {
+        messageElement.innerText = "Mi dispiace, non sei in lista."
+    }
 
 })
